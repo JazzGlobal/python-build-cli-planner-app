604b9fdef1feaece4cab93304db71efc74f558b2
Christopher Gambrell <waymer147@gmail.com>
2021-06-27 14:49:33 -0400

    Added issubclass again ...?

--- src/database.py
+++ src/database.py
@@ -17,7 +17,7 @@ def list_reminders():
 
 def add_reminder(text, date, ReminderClass):
     reminder = ReminderClass(text, date)
-    if isinstance(reminder, DeadlinedReminder):
+    if isinstance(reminder, DeadlinedReminder) or issubclass(ReminderClass, DeadlinedReminder):
         with open('reminders.csv', 'a+', newline='\n') as file:
             writer = csv.writer(file)
             writer.writerow(reminder)
